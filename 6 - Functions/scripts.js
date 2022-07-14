b0ef3d571b2064d5c8b9555a3f453c57ab173373
lipefreire <felipef12123@gmail.com>
2022-07-14 10:08:19 -0300

    Functions

---
+++ 6 - Functions/scripts.js
@@ -0,0 +1,9 @@
+let corSite = "azul";
+
+function resetaCor(cor, tonalidade) {
+    corSite = cor + ' ' + tonalidade;
+};
+
+console.log(corSite);
+resetaCor("Verde", "claro")
+console.log(corSite);
