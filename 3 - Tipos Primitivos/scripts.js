1379092aa228187aa0ad93b7090e288c4e6369a7
lipefreire <felipef12123@gmail.com>
2022-07-14 09:36:04 -0300

    Tipos primitivos

---
+++ 3 - Tipos Primitivos/scripts.js
@@ -0,0 +1,5 @@
+let nome = 'Felipe'; //STRING LITERAL
+let idade = 20; //NUMBER LITERAL
+let estaAprovado = true; //BOOLEAN LITERAL
+let sobrenome; //UNDEFINED 
+let corSelecionada = null //REDEFINIR UM VALOR
