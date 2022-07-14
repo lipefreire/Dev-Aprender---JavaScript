774f744a3552b571952d8185bff726fafeadc53c
lipefreire <felipef12123@gmail.com>
2022-07-14 09:45:47 -0300

    objetos

---
+++ 4 - Objetos/scripts.js
@@ -0,0 +1,14 @@
+let nome = 'Felipe'; 
+let idade = 20; 
+let estaAprovado = true; 
+let sobrenome; 
+let corSelecionada = null
+
+let pessoa = {
+    nome: 'Felipe',
+    idade: 20,
+    estaAprovado: true,
+    sobrenome: 'Freire'
+};
+
+console.log(pessoa);
