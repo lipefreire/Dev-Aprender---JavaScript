4534787e44c19241119c1a542c454e0365b6d89b
lipefreire <felipef12123@gmail.com>
2022-07-14 09:30:49 -0300

    Variaveis e constantes

---
+++ 1 - Variaveis/1.scripts.js
@@ -0,0 +1,8 @@
+let idade = 5;
+console.log(idade)
+
+let altura = 180;
+console.log(altura);
+
+let nome = 'Felipe';
+console.log(nome);
---
+++ 2 - Constantes/scripts.js
@@ -0,0 +1,4 @@
+const valorIngressoAdulto = 30;
+//ISSO NÃO PODE ACONTECER POIS O VALOR É CONSTANTE(O VALOR NÃO SE ALTERA)
+valorIngressoAdulto = 40
+console.log(valorIngressoAdulto)
