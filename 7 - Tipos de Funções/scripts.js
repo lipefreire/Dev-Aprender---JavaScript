0d8e0488ad82d1c320c5775003294bd5ef9cb3ed
lipefreire <felipef12123@gmail.com>
2022-07-15 06:56:55 -0300

    Tipos de funções

---
+++ 7 - Tipos de Funções/scripts.js
@@ -0,0 +1,16 @@
+//REALIZAR UMA TAREFA, NÃO DEVOLVE NADA
+function dizerNome() {
+    console.log('Felipe');
+}
+
+dizerNome();
+
+//FAZ UM CÁLCULO OU OPERAÇÃO E RETORNA ALGO
+function multiplicarPorDois(valor) {
+    return valor * 2;
+}
+
+console.log(multiplicarPorDois(5));
+
+let resultado = multiplicarPorDois(5);
+console.log(resultado);
