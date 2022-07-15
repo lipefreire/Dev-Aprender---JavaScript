973ee41e8b5599852cfd50d10d540cbd49829206
lipefreire <felipef12123@gmail.com>
2022-07-15 07:07:12 -0300

    operadores de atribuição

---
+++ 8 - Operadores/2 - Operador de Atribuição/scripts.js
@@ -0,0 +1,8 @@
+let valorTecladoGamer = 100;
+// VALOR = VALOR + VALOR
+valorTecladoGamer += valorTecladoGamer;
+
+// VALOR = VALOR - VALOR
+valorTecladoGamer -= valorTecladoGamer
+
+console.log(valorTecladoGamer);
