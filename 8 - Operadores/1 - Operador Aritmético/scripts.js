24cb708a6d0b5467c37f3ba62cc17269cd5004ce
lipefreire <felipef12123@gmail.com>
2022-07-15 07:04:06 -0300

    operador aritmético

---
+++ 8 - Operadores/1 - Operador Aritmético/scripts.js
@@ -0,0 +1,15 @@
+//OPERADORES ARITMÉTICOS
+// +, -, *, /, **
+let salario = 100;
+console.log(salario + salario);
+console.log(salario - salario);
+console.log(salario * salario);
+console.log(salario / salario);
+console.log(5 ** 5)
+
+// ++ e -- SÃO OPERADORES DE INCREMENTO E DECREMENTO
+
+let idade = 20;
+
+console.log(idade++);
+console.log(idade);
