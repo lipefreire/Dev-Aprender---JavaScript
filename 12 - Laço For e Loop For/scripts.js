2612ed7f020377f470b9d34317630ee1951b1f81
lipefreire <felipef12123@gmail.com>
2022-07-17 05:02:19 -0300

    Laço for e loop for

---
+++ 12 - Laço For e Loop For/scripts.js
@@ -0,0 +1,17 @@
+// 1. For
+// 2. While
+// 3. Do.. While
+// 4. For.. In
+// 5. For.. Of
+
+// Para números ímpares
+for(let i = 0; i <= 5; i++) {
+    if(i % 2 !== 0){
+        console.log(i);
+    }
+}
+
+//Forma decrescente
+for(let i = 5; i >= 1; i--){
+    console.log(i);
+}
